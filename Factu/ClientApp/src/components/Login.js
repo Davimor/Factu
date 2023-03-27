5dfbbbda132c88db617f007c12028f248cf4d383
Davimor <davimorsp@gmail.com>
2023-03-27 23:13:18 +0200

    Css aligbn button

--- Factu/ClientApp/src/components/Login.js
+++ Factu/ClientApp/src/components/Login.js
@@ -44,9 +44,9 @@ export const Login = () => {
                 <p>{user.Logged}</p>
                 <div className="row d-flex align-items-center justify-content-center h-100">
                     <div className="col-md-8 col-lg-7 col-xl-6">
-                        <img src={'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg'} className="img-fluid" alt="login-img"/>
+                        <img src={'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg'} className="img-fluid" alt="login-img" />
                     </div>
-                    <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                    <div className="col-md-7 col-lg-5 col-xl-5" style={{ 'display': 'flex', 'align-items': 'center', 'justify-content': 'center' }}>
                         <form>
                             <button type="button" className="btn btn-primary btn-lg btn-block" onClick={() => login()}>!Accede!</button>
                         </form>
